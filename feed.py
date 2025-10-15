c425fb131fffe630bc7f8d9e8d2c1d781aaa0f2c
ianlikescoding <71194415+ianlikescoding@users.noreply.github.com>
2025-10-15 14:46:48 -0400

    Update feed.py

--- feed.py
+++ feed.py
@@ -1,6 +1,6 @@
 import yaml
 import xml.etree.ElementTree as xml_tree
-
+#test
 with open('feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
     rss_element = xml_tree.Element('rss', {'version':'2.0',
